5c8eac1758d78fe966b225542eba1b698f3a7b80
ctwc55 <ssy990408@gmail.com>
2023-02-20 19:13:55 +0900

    init project

---
+++ src/main/java/oasisbot24/oasisapi/OasisApiApplication.java
@@ -0,0 +1,13 @@
+package oasisbot24.oasisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oasisbot24/oasisapi/OasisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package oasisbot24.oasisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
