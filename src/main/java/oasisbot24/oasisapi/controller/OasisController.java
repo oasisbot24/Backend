299f40efed9b93428e29674f7db2090d0bb919f7
ctwc55 <ssy990408@gmail.com>
2023-03-02 22:51:31 +0900

    update OasisController.java

--- src/main/java/oasisbot24/oasisapi/controller/OasisController.java
+++ src/main/java/oasisbot24/oasisapi/controller/OasisController.java
@@ -1,20 +1,48 @@
 package oasisbot24.oasisapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import oasisbot24.oasisapi.jwt.TokenProvider;
+import oasisbot24.oasisapi.service.SignInService;
 import oasisbot24.oasisapi.service.TestService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import oasisbot24.oasisapi.service.UserService;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class OasisController {
 
     private final TestService testService;
+    private final SignInService signInService;
+    private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    @GetMapping("/api/test")
+    @GetMapping("/test")
     public Map<String, Object> testController() {
         return testService.getTestData();
+    }
+
+    @PostMapping("/signin")
+    public Map<String, Object> signInController() {
+        return signInService.signIn();
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> userInfoController() {
+        return userService.getUserData();
+    }
+
+    @PostMapping("/user")
+    public void signUpController(@RequestBody Map<String, Object> payload) {
+        userService.signUpUser(payload);
+    }
+
+    @GetMapping("/signup")
+    public void emailVerificationController(@RequestParam("email") String email, @RequestParam("auth") String auth) {
+        userService.emailVerification(email, auth);
     }
 }
