84c9721ad4ad98ef1b8c032ddf0391c419f44932
ctwc55 <ssy990408@gmail.com>
2023-03-02 20:30:23 +0900

    add dependencies related swagger and gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,11 @@ dependencies {
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("mysql:mysql-connector-java")
+	implementation("io.springfox:springfox-boot-starter:3.0.0") //추후 Spring-Doc 적용 시도
+	implementation("com.google.code.gson:gson")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-	implementation("mysql:mysql-connector-java")
-	implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
