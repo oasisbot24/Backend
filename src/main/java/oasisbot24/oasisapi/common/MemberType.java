bc92a3497bf6b0c8e4d45486a46738c995e8ce6d
ctwc55 <ssy990408@gmail.com>
2023-03-07 14:01:33 +0900

    create MemberType.java

---
+++ src/main/java/oasisbot24/oasisapi/common/MemberType.java
@@ -0,0 +1,10 @@
+package oasisbot24.oasisapi.common;
+
+import org.springframework.stereotype.Component;
+
+public class MemberType {
+    public static final Integer UNVERIFIED = 0;
+    public static final Integer ADMIN = 1;
+    public static final Integer NORMAL = 2;
+    public static final Integer WITHDRAWAL = 3;
+}
