875a935f9462c046806181d534be02e9cc9dbb6a
ctwc55 <ssy990408@gmail.com>
2023-03-10 00:20:33 +0900

    update type

--- src/main/java/oasisbot24/oasisapi/common/MemberType.java
+++ src/main/java/oasisbot24/oasisapi/common/MemberType.java
@@ -3,8 +3,8 @@ package oasisbot24.oasisapi.common;
 import org.springframework.stereotype.Component;
 
 public class MemberType {
-    public static final Integer UNVERIFIED = 0;
-    public static final Integer ADMIN = 1;
-    public static final Integer NORMAL = 2;
-    public static final Integer WITHDRAWAL = 3;
+    public static final int UNVERIFIED = 0;
+    public static final int ADMIN = 1;
+    public static final int NORMAL = 2;
+    public static final int WITHDRAWAL = 3;
 }
