7218c88cb3f9e7aeea7c7fd8000f9b7b89b052f6
ctwc55 <ssy990408@gmail.com>
2023-03-10 00:19:22 +0900

    update token-validity-in-seconds to token-validity-in-milliseconds

--- src/main/java/oasisbot24/oasisapi/jwt/TokenProvider.java
+++ src/main/java/oasisbot24/oasisapi/jwt/TokenProvider.java
@@ -37,7 +37,7 @@ public class TokenProvider implements InitializingBean {
     private Key key;
 
     public TokenProvider(@Value("${jwt.secret}") String secret,
-                         @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds){
+                         @Value("${jwt.token-validity-in-milliseconds}") long tokenValidityInMilliseconds){
         this.secret = secret;
         this.tokenValidityInMilliseconds = tokenValidityInMilliseconds;
     }
