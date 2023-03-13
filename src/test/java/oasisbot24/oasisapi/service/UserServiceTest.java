7e5ce32948d58cf03d27b01dc311bfc30b0afe15
ctwc55 <ssy990408@gmail.com>
2023-03-14 02:20:39 +0900

    update UserServiceTest.java

--- src/test/java/oasisbot24/oasisapi/service/UserServiceTest.java
+++ src/test/java/oasisbot24/oasisapi/service/UserServiceTest.java
@@ -29,9 +29,22 @@ public class UserServiceTest {
         payload.put("nick_name", "test");
 
         //when
-        ResponseEntity<EmailDupCheckVO> IsEmailSend = userService.signUpUser(payload);
+        ResponseEntity<EmailDupCheckVO> isEmailSend = userService.signUpUser(payload);
 
         //then
-        assertThat(IsEmailSend.getBody().getIs_duplicated()).isEqualTo(IsEmailDuplicated.NO);
+        assertThat(isEmailSend.getBody().getIs_duplicated()).isEqualTo(IsEmailDuplicated.NO);
+    }
+
+    @Test
+    void 이메일인증() {
+        //given
+        String email = "test@test.com";
+        String auth = "8e6413a4-c61d-4540-94e1-19a65dda52a1";
+
+        //when
+        String isVerified = userService.emailVerification(email, auth);
+
+        //then
+        assertThat(isVerified).isEqualTo("이메일 인증이 완료되었습니다!");
     }
 }
