69cb7ad11f4106d8a2fc1982683e1e7db49d98df
ctwc55 <ssy990408@gmail.com>
2023-03-14 02:14:57 +0900

    update OasisController.java

--- src/main/java/oasisbot24/oasisapi/controller/OasisController.java
+++ src/main/java/oasisbot24/oasisapi/controller/OasisController.java
@@ -1,11 +1,13 @@
 package oasisbot24.oasisapi.controller;
 
 import lombok.RequiredArgsConstructor;
-import oasisbot24.oasisapi.jwt.TokenProvider;
+import oasisbot24.oasisapi.domain.EmailDupCheckVO;
+import oasisbot24.oasisapi.domain.SignInVO;
+import oasisbot24.oasisapi.jwt.TokenDTO;
 import oasisbot24.oasisapi.service.SignInService;
 import oasisbot24.oasisapi.service.TestService;
 import oasisbot24.oasisapi.service.UserService;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -18,8 +20,6 @@ public class OasisController {
     private final TestService testService;
     private final SignInService signInService;
     private final UserService userService;
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @GetMapping("/test")
     public Map<String, Object> testController() {
@@ -27,22 +27,22 @@ public class OasisController {
     }
 
     @PostMapping("/signin")
-    public Map<String, Object> signInController() {
-        return signInService.signIn();
+    public ResponseEntity<TokenDTO> signInController(@RequestBody SignInVO signInVO) {
+        return signInService.signIn(signInVO);
     }
 
     @GetMapping("/user")
-    public Map<String, Object> userInfoController() {
+    public Map<String, Object> userInfoController(@RequestHeader String data) {
         return userService.getUserData();
     }
 
     @PostMapping("/user")
-    public void signUpController(@RequestBody Map<String, Object> payload) {
-        userService.signUpUser(payload);
+    public ResponseEntity<EmailDupCheckVO> signUpController(@RequestBody Map<String, Object> payload) {
+        return userService.signUpUser(payload);
     }
 
     @GetMapping("/signup")
-    public void emailVerificationController(@RequestParam("email") String email, @RequestParam("auth") String auth) {
-        userService.emailVerification(email, auth);
+    public String emailVerificationController(@RequestParam("email") String email, @RequestParam("auth") String auth) {
+        return userService.emailVerification(email, auth);
     }
 }
