9b679a62e67761e6300feafa982c0e8cb580eac1
ctwc55 <ssy990408@gmail.com>
2023-03-14 12:07:12 +0900

    remove type for withdrawal

--- src/main/java/oasisbot24/oasisapi/common/MemberType.java
+++ src/main/java/oasisbot24/oasisapi/common/MemberType.java
@@ -4,5 +4,4 @@ public class MemberType {
     public static final int UNVERIFIED = 0;
     public static final int ADMIN = 1;
     public static final int NORMAL = 2;
-    public static final int WITHDRAWAL = 3;
 }
